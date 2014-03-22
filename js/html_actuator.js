bb1fe485594c2b69b6870e356fcf4c74f197cae6
Jamil Dhanani <jamil.dhanani@gmail.com>
2014-03-22 12:35:24 -0400

    Do these look like the faces of mercy?
    (changed the "You Lose!" message)

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -126,7 +126,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Do these look like the faces of mercy?";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
